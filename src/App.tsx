9bcb1186136c96096d0754e38bb5c9ff7269ca97
Kirill <kirilldyachkovskiy@gmail.com>
2022-07-05 22:48:52 +0300

    add linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import {FC} from 'react'
+import { FC } from 'react'
 import './App.css'
 
-const App: FC = () => {
-  return <div>Lol</div>
-}
+const App: FC = () => <div>Lol</div>
 
 export default App
